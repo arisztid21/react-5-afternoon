483abf398b002d01f60f0d7204be70e843bd9bab
arisztid munoz <arisztidmunoz@arisztids-MacBook-Pro.local>
2018-08-27 17:38:01 -0700

    wpx7 done

--- src/ducks/reducer.js
+++
@@ -1,5 +0,0 @@
-function reducer(){
-
-}
-
-export default reducer;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<BrowserRouter><Provider store={store}><App /></Provider></BrowserRouter>, document.getElementById('root'));
+// registerServiceWorker();
 
